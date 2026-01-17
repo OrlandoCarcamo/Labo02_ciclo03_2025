1be0c5694610ae69379ac3144c96a0bfee6f6e3a
OrlandoCarcamo <00370825@uca.edu.sv>
2026-01-16 19:27:28 -0600

    add; metodo imprimir agregado

--- Listaes.cpp
+++ Listaes.cpp
@@ -13,11 +13,16 @@ class estudiante{
         notaFinal = nF; 
 
     }
+    void imprimir (){
+        cout << "Nombre: " << nombre << endl << "Nota final: " << notaFinal << endl;
+    }
+     
     
 
      };
 
 int main (){
-
+ estudiante n1("Orlando", 9.5);
+    n1.imprimir();
     return 0;
 }
